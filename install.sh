b729305065569065ea52d7ceab897cbabad20001
Pete Saia <pete@petesaia.com>
2017-09-30 23:04:16 -0400

    Fixed issue in install script

--- install.sh
+++ install.sh
@@ -3,17 +3,26 @@
 KERNEL=$(uname -s)
 ARCH=$(uname -m)
 
-if [ $KERNEL = "Darwin" ]; then
+if [ $KERNEL = "Darwin" ]
+then
 	BINARY="https://github.com/LevInteractive/allwrite-docs/blob/master/build/darwin_amd64.tar.gz?raw=true"
-elif [ $Nucleo = "Linux" ]; then
-	if [$ARCH = "x86_64"; then
+elif [ $Nucleo = "Linux" ]
+then
+	if [$ARCH = "x86_64"]
+	then
 		BINARY="https://github.com/LevInteractive/allwrite-docs/blob/master/build/linux_amd64.tar.gz?raw=true"
-	elif [$ARCH = "x86"; then
+	elif [$ARCH = "x86"]
+	then
 		BINARY="https://github.com/LevInteractive/allwrite-docs/blob/master/build/linux_386.tar.gz?raw=true"
-	elif [ $ARCH = "i386" ]; then
+	elif [ $ARCH = "i386" ]
+	then
 		BINARY="https://github.com/LevInteractive/allwrite-docs/blob/master/build/linux_386.tar.gz?raw=true"
-	elif [$ARCH = "i686"; then
+	elif [$ARCH = "i686"]
+	then
 		BINARY="https://github.com/LevInteractive/allwrite-docs/blob/master/build/linux_386.tar.gz?raw=true"
+	else
+		echo "Unsupported OS: $ARCH"
+		exit 1
 	fi
 else
 	echo "Unsupported OS: $ARCH"
